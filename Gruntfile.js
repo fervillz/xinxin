71cb419160ec963b5f1de6bfcbceb2e7e7ae619b
Fernando <fervillz@gmail.com>
2016-01-10 10:52:54 -0800

    first files

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function (grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			dist: {
+				options: {
+					style: 'compressed',
+					sourcemap: 'none',
+					cacheLocation: 'sass/.cache-location',
+				},
+				files: [{
+					'style.css': 'sass/style.scss',
+				}],
+			},
+		},
+
+		autoprefixer:{
+			dist: {
+				files: {
+					'style.css': 'style.css',
+				},
+			},
+		},
+
+		watch: {
+			options: {
+				livereload: true,
+			},
+			scripts: {
+				files: ['**/*.js'],
+				tasks: ['jshint'],
+			},
+			css: {
+				files: '**/*.scss',
+				tasks: ['sass', 'autoprefixer'],
+			},
+		},
+
+		jshint: {
+			all: ['Gruntfile.js', 'scripts/scripts.js'],
+		},
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-autoprefixer');
+
+	grunt.registerTask('default', ['watch']);
+	grunt.registerTask('css', ['sass', 'autoprefixer']);
+};
---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package xinxin
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'xinxin' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'xinxin' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'xinxin' ), 'xinxin', '<a href="http://fernandovillamorjr.com" rel="designer">fernan villamor jr</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
